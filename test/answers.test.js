5f26b441baed9f6c08aaf2613a8cd696276a8283
alessandroconti97 <alessandro.conti.97@gmail.com>
2018-12-01 10:47:07 +0100

    Move /lib/*.test.js in /test (issue #62)

--- test/answers.test.js
+++ test/answers.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('./app');
+const app = require('../lib/app');
 
 test('POST /answers should return 201', (done) => {
     let payload = {
--- test/tasks.test.js
+++ test/tasks.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('./app');
+const app = require('../lib/app');
 
 test('POST /tasks should return 201 and the created task', (done) => {
     let payload = {
