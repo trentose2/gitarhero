cfc94a00eec89d8d7ae5764aaeeb654fecc14cb6
Giovanni <giotra16@gmail.com>
2018-12-01 19:17:30 +0100

    add tests for PUT /groups/:id and /groups/:id/members

--- test/groups.test.js
+++ test/groups.test.js
@@ -1,13 +1,11 @@
 const request = require('supertest');
 const app = require('../lib/app');
 
-// post /groups test
+// POST /groups tests
 test('POST /groups should return 201 and create a group', (done) => {
     let payload = {
         name: 'group name',
-        members: [
-            1, 2, 3, 4
-        ]
+        members: [ 1, 2, 3, 4 ]
     }
 
     request(app)
@@ -76,7 +74,7 @@ test('POST /groups should return 400 when "members" contains invalid values', (d
     }
 
     request(app)
-        .post('/api/v1/assignments')
+        .post('/api/v1/groups')
         .send(payload)
         .set('Content-Type', 'application/json')
         .set('Accept', 'application/json')
@@ -90,7 +88,7 @@ test('POST /groups should return 400 when "members" contains invalid values', (d
         });
 });
 
-// get /groups tests
+// GET /groups tests
 test('GET /groups should return an array of groups', (done) => {
     request(app)
         .get('/api/v1/groups')
@@ -107,11 +105,11 @@ test('GET /groups should return an array of groups', (done) => {
         });
 });
 
-// put /groups/:id
+// PUT /groups/:id tests
 test('PUT /groups/:id should return 200 when group is updated', (done) => {
     
     let payload = {
-        name : "group",
+        name : "group name",
         members : [1, 2, 3, 4]
     }
     
@@ -128,6 +126,247 @@ test('PUT /groups/:id should return 200 when group is updated', (done) => {
                 console.log(res.error);
             }
            
+            done(err);
+        });
+});
+
+test('PUT /groups/:id should return 400 when "name" is not a string', (done) => {
+    let payload = {
+        name: 123,
+        members: [ 1, 2, 3, 4 ]
+    }
+
+    let id = 1;
+
+    request(app)
+        .put('/api/v1/groups/' + id)
+        .send(payload)
+        .set('Content-Type', 'application/json')
+        .set('Accept', 'application/json')
+        .expect(400)
+        .end((err, res) => {
+            if (err && res.error) {
+                console.log(res.error);
+            }
+
+            done(err);
+        });
+});
+
+test('PUT /groups/:id should return 400 when "members" contains invalid values', (done) => {
+    let payload = {
+        name: "group name",
+        members: [ false, "a", null, -1 ]
+    }
+
+    let id = 1;
+
+    request(app)
+        .post('/api/v1/groups/' + id)
+        .send(payload)
+        .set('Content-Type', 'application/json')
+        .set('Accept', 'application/json')
+        .expect(400)
+        .end((err, res) => {
+            if (err && res.error) {
+                console.log(res.error);
+            }
+
+            done(err);
+        });
+});
+
+test('PUT /groups/:id should return 400 when "name" is missing', (done) => {
+    let payload = {
+        // name: "group name",
+        members: [ 1, 2, 3, 4 ]
+    }
+
+    let id = 1;
+
+    request(app)
+        .put('/api/v1/groups' + id)
+        .send(payload)
+        .set('Content-Type', 'application/json')
+        .set('Accept', 'application/json')
+        .expect(400)
+        .end((err, res) => {
+            if (err && res.error) {
+                console.log(res.error);
+            }
+
+            done(err);
+        });
+});
+
+test('PUT /groups/:id should return 400 when "members" is missing', (done) => {
+    let payload = {
+        name: "group name",
+        // members: [ 1, 2, 3, 4 ]
+    }
+
+    let id = 1;
+
+    request(app)
+        .put('/api/v1/groups' + id)
+        .send(payload)
+        .set('Content-Type', 'application/json')
+        .set('Accept', 'application/json')
+        .expect(400)
+        .end((err, res) => {
+            if (err && res.error) {
+                console.log(res.error);
+            }
+
+            done(err);
+        });
+});
+
+test('PUT /groups/:id should return 400 when id is 0', (done) => {
+    
+    let payload = {
+        name : "group name",
+        members : [1, 2, 3, 4]
+    }
+    
+    let id = 0;
+
+    request(app)
+        .put('/api/v1/groups/' + id)
+        .send(payload)
+        .set('Content-Type', 'application/json')
+        .set('Accept', 'application/json')
+        .expect(400)
+        .end((err, res) => {
+            if(err && res.error){
+                console.log(res.error);
+            }
+           
+            done(err);
+        });
+});
+
+test('PUT /groups/:id should return 400 when id is less than 0', (done) => {
+    
+    let payload = {
+        name : "group name",
+        members : [1, 2, 3, 4]
+    }
+    
+    let id = -2;
+
+    request(app)
+        .put('/api/v1/groups/' + id)
+        .send(payload)
+        .set('Content-Type', 'application/json')
+        .set('Accept', 'application/json')
+        .expect(400)
+        .end((err, res) => {
+            if(err && res.error){
+                console.log(res.error);
+            }
+           
+            done(err);
+        });
+});
+
+test('PUT /groups/:id should return 404 if there is not a group with that id', (done) => {
+    
+    let payload = {
+        name : "group name",
+        members : [1, 2, 3, 4]
+    }
+    
+    // let id = storage.groups[storage.groups.length].id;
+    let id = 100;
+
+    request(app)
+        .put('/api/v1/groups/' + id)
+        .send(payload)
+        .set('Content-Type', 'application/json')
+        .set('Accept', 'application/json')
+        .expect(404)
+        .end((err, res) => {
+            if(err && res.error){
+                console.log(res.error);
+            }
+           
+            done(err);
+        });
+});
+
+// GET /groups/:id/members tests
+
+test('GET /groups/:id/members should return 200 and an array of members', (done) => {
+    
+    let id = 1;
+
+    request(app)
+        .get('/api/v1/groups/' + id + '/members')
+        .set('Content-Type', 'application/json')
+        .set('Accept', 'application/json')
+        .expect(200)
+        .end((err, res) => {
+            if(err && res.error){
+                console.log(res.error);
+            }
+
+            expect(Array.isArray(res.body)).toBe(true);
+            done(err);
+        });
+});
+
+test('GET /groups/:id/members should return 400 if id is 0', (done) => {
+    
+    let id = 0;
+
+    request(app)
+        .get('/api/v1/groups/' + id + '/members')
+        .set('Content-Type', 'application/json')
+        .set('Accept', 'application/json')
+        .expect(400)
+        .end((err, res) => {
+            if(err && res.error){
+                console.log(res.error);
+            }
+
+            done(err);
+        });
+});
+
+test('GET /groups/:id/members should return 400 if id is less than 0', (done) => {
+    
+    let id = -2;
+
+    request(app)
+        .get('/api/v1/groups/' + id + '/members')
+        .set('Content-Type', 'application/json')
+        .set('Accept', 'application/json')
+        .expect(400)
+        .end((err, res) => {
+            if(err && res.error){
+                console.log(res.error);
+            }
+
+            done(err);
+        });
+});
+
+test('GET /groups/:id/members should return 404 if there is not a group with that id', (done) => {
+    
+    // let id = storage.groups[storage.groups.length].id;
+    let id = 100;
+
+    request(app)
+        .get('/api/v1/groups/' + id + '/members')
+        .set('Content-Type', 'application/json')
+        .set('Accept', 'application/json')
+        .expect(404)
+        .end((err, res) => {
+            if(err && res.error){
+                console.log(res.error);
+            }
+
             done(err);
         });
 });
